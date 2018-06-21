f9aa2948a304b8790b64adb8e7b9e7e9b4a2e761
VirginieGodfrin <virginiegodfrin@gmail.com>
2018-06-21 17:05:09 +0200

    Chapter 6: Customizing the Forms

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -16,7 +16,36 @@ class User extends BaseUser
 	 */
 	protected $id;
 
+	/**
+	 * @ORM\Column(type="string", nullable=true)
+	 */
+	protected $firstname;
+
 	public function getId() {
 		return $this->id; 
 	}
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     *
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
 }
